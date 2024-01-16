863a38bf263b38d06f1b20b4d4a9cbb41a1f1529
Jeremy <jdavid.rupprecht@gmail.com>
2024-01-16 13:55:06 -0700

    Create basic data processing functions

--- src/index.js
+++ src/index.js
@@ -22,23 +22,78 @@ function component() {
 }
 document.body.appendChild(component());
 
-async function getWeatherForLocation(location) {
+async function fetchWeatherData(requestParameters, location) {
   try {
     const weatherDataResponse = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=ba28fb17cfeb4cddac8203519241301&q=${location}`,
+      `http://api.weatherapi.com/v1/${requestParameters.type}.json?key=ba28fb17cfeb4cddac8203519241301&q=${location}${requestParameters.days}`,
       { mode: "cors" },
     );
     const weatherDataJSON = await weatherDataResponse.json();
-    console.log(weatherDataJSON);
+    // console.table(weatherDataJSON);
     return weatherDataJSON;
   } catch (error) {
-    console.log("Error while fetching weather data", error);
+    console.log("Error while fetching forecast weather data", error);
     return error;
   }
 }
 
-const promise = getWeatherForLocation("Calgary");
-console.log(promise);
+// Upper left corner data
+async function processDataForUpperLeft(forecastDataPromise) {
+  const currentData = await forecastDataPromise;
+  const data = {
+    condition: currentData.current.condition.text,
+    location: currentData.location.name,
+    date: currentData.location.localtime.split(" ")[0],
+    time: currentData.location.localtime.split(" ")[1],
+    temperateC: currentData.current.temp_c,
+    temperateF: currentData.current.temp_f,
+    conditionIcon: currentData.current.condition.icon,
+  };
+  return data;
+}
+
+// Upper right corner data. Forecast data is required because the "current"
+// data doesn't include the daily "chance of rain" which needs to be displayed
+async function processDataForUpperRight(forecastDataPromise) {
+  const forecastData = await forecastDataPromise;
+  const data = {
+    feelsLikeC: forecastData.current.feelslike_c,
+    feelsLikeF: forecastData.current.feelslike_f,
+    humidity: forecastData.current.humidity,
+    chanceOfRain: forecastData.forecast.forecastday[0].day.daily_chance_of_rain,
+    chanceOfSnow: forecastData.forecast.forecastday[0].day.daily_chance_of_snow,
+    windSpeedKmHr: forecastData.current.gust_kph,
+    windSpeedMph: forecastData.current.gust_mph,
+  };
+  return data;
+}
+
+// Footer data
+// async function processDailyDataForFooter(historicalDataPromise, forecastDataPromise) {
+//   const historicalData = await historicalDataPromise;
+//   const forecastData = await forecastDataPromise;
+
+// }
+
+// const currentData = fetchWeatherData({ type: "current", days: "" }, "Calgary");
+const forecastData = fetchWeatherData(
+  { type: "forecast", days: "&days=3" },
+  "Calgary",
+);
+// const historicalData = fetchWeatherData({type: "history"});
+processDataForUpperLeft(forecastData);
+processDataForUpperRight(forecastData);
+
+// function showDataForUpperLeft(data) {}
+// function showDataForUpperRight(data) {}
+
+// async function processHourlyDataForFooter(dataPromise) {
+//   const data = undefined;
+//   return data;
+// }
+
+// function showDailyDataForFooter(data) {}
+// function showHourlyDataForFooter(data) {}
 
 /*
 Upper Left Corner:
