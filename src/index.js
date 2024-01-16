7dec6a0bc4cbba8a0d64692e0958b206ed2a4086
Jeremy <jdavid.rupprecht@gmail.com>
2024-01-15 23:45:42 -0700

    Create skeleton with basic weather object fetching and commented project structure

--- src/index.js
+++ src/index.js
@@ -21,3 +21,46 @@ function component() {
   return element;
 }
 document.body.appendChild(component());
+
+async function getWeatherForLocation(location) {
+  try {
+    const weatherDataResponse = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=ba28fb17cfeb4cddac8203519241301&q=${location}`,
+    );
+    const weatherDataJSON = await weatherDataResponse.json();
+    console.log(weatherDataJSON);
+    return weatherDataJSON;
+  } catch (error) {
+    console.log("Error while fetching weather data", error);
+    return error;
+  }
+}
+
+const promise = getWeatherForLocation("Calgary");
+console.log(promise);
+
+/*
+Upper Left Corner:
+
+Overall Weather
+Location
+Date
+Time
+Temperature
+Button to Switch between F and C
+Icon based on the overall weather for today
+Location Searchbar
+
+Upper Right Corner:
+Feels like
+Humidity
+Chance of Rain
+Wind Speed
+
+Footer:
+Daily/Hourly button
+Weather For each day, includes:
+  temperature
+  Icon showing the overall weather for that day
+
+*/
