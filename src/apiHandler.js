b476d9ff808336246076cb46c863c5f8046ec24c
Jeremy <jdavid.rupprecht@gmail.com>
2024-01-16 23:56:37 -0700

    Move all weather data api methods into their own module, and create skeleton file for dom methods

---
+++ src/apiHandler.js
@@ -0,0 +1,87 @@
+async function getLocationCoordinates(location) {
+  try {
+    const coordinatesPromse = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${location}&count=1&language=en&format=json`,
+    );
+    const coordinatesObject = await coordinatesPromse.json();
+    if (coordinatesObject.results) {
+      const { name, latitude, longitude, timezone } =
+        coordinatesObject.results[0];
+      return { name, latitude, longitude, timezone };
+    }
+    throw new Error("Error fetching location coordiantes");
+  } catch (error) {
+    throw new Error("Error fetching location coordiantes", error);
+  }
+}
+
+async function buildFetchURL(
+  coordinatePromise,
+  currentOrForecast,
+  celciusOrFahrenheit,
+) {
+  const coordinateData = await coordinatePromise;
+
+  // Current data in Celcius
+  let url = `https://api.open-meteo.com/v1/forecast?latitude=${coordinateData.latitude}&longitude=${coordinateData.longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&hourly=temperature_2m,weather_code&forecast_days=1&timezone=${coordinateData.timezone}`;
+
+  // Current data in Fahrenheit
+  if (currentOrForecast === "Current" && celciusOrFahrenheit === "Fahrenheit") {
+    url = `https://api.open-meteo.com/v1/forecast?latitude=${coordinateData.latitude}&longitude=${coordinateData.longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&hourly=temperature_2m,weather_code&forecast_days=1&timezone=${coordinateData.timezone}&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch`;
+
+    // Forecast data in Celcius
+  } else if (
+    currentOrForecast === "Forecast" &&
+    celciusOrFahrenheit === "Celcius"
+  ) {
+    url = `https://api.open-meteo.com/v1/forecast?latitude=${coordinateData.latitude}&longitude=${coordinateData.longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min&forecast_days=7&timezone=${coordinateData.timezone}`;
+
+    // Forecast data in Fahrenheit
+  } else if (
+    currentOrForecast === "Forecast" &&
+    celciusOrFahrenheit === "Fahrenheit"
+  ) {
+    url = `https://api.open-meteo.com/v1/forecast?latitude=${coordinateData.latitude}&longitude=${coordinateData.longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min&forecast_days=7&timezone=${coordinateData.timezone}&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch`;
+  }
+  return url;
+}
+
+async function fetchCurrentWeatherData(urlPromise) {
+  const url = await urlPromise;
+  try {
+    const weatherDataResponse = await fetch(url, { mode: "cors" });
+    if (!weatherDataResponse.error) {
+      const weatherDataJSON = await weatherDataResponse.json();
+      return weatherDataJSON;
+    }
+    throw new Error("Error while fetching weather data");
+  } catch (error) {
+    throw new Error("Error while fetching weather data", error);
+  }
+}
+
+async function getWeatherData(location) {
+  try {
+    const coordinates = await getLocationCoordinates(location);
+    const url1 = buildFetchURL(coordinates, "Current", "Celcius");
+    const url2 = buildFetchURL(coordinates, "Current", "Fahrenheit");
+    const url3 = buildFetchURL(coordinates, "Forecast", "Celcius");
+    const url4 = buildFetchURL(coordinates, "Forecast", "Fahrenheit");
+
+    const allWeatherData = await Promise.all([
+      fetchCurrentWeatherData(url1),
+      fetchCurrentWeatherData(url2),
+      fetchCurrentWeatherData(url3),
+      fetchCurrentWeatherData(url4),
+    ]);
+
+    console.log(allWeatherData);
+    return allWeatherData;
+  } catch (error) {
+    console.log("Error while getting weather data", error);
+    return error;
+    // throw new Error("Error while getting weather data", error);
+  }
+}
+
+export default getWeatherData;
