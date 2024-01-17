b95cc6fa9d18efc52ca7c36fdc817612f527bdfb
Jeremy <jdavid.rupprecht@gmail.com>
2024-01-17 16:22:41 -0700

    Create HTML structure and styling for the upper left and upper right displays

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import "./style.css";
+import searchIcon from "./images/searchIcon.svg";
+import feelsLikeIcon from "./images/feelsLikeIcon.svg";
+import humidityIcon from "./images/humidityIcon.svg";
+import precipitationIcon from "./images/precipitationIcon.svg";
+import windSpeedIcon from "./images/windIcon.svg";
 // import backgroud from "./images/weatherBackground.jpg";
 // import testImage from "./images/testImage.png";
-// import getWeatherData from "./apiHandler";
+import getWeatherData from "./apiHandler";
 
 // function component() {
 //   // Test CSS
@@ -24,4 +29,29 @@ import "./style.css";
 // }
 // document.body.appendChild(component());
 
-// getWeatherData("new york city");
+function renderImage(parent, image) {
+  const imageElement = new Image();
+  imageElement.src = image;
+  parent.appendChild(imageElement);
+}
+
+function renderIcons() {
+  // Searchbar icon
+  const searchIconContainer = document.getElementById("searchIconContainer");
+  renderImage(searchIconContainer, searchIcon);
+
+  // Upper right icons
+  const feelsLikeContainer = document.getElementById("feelsLikeIcon");
+  renderImage(feelsLikeContainer, feelsLikeIcon);
+
+  const humidityContainer = document.getElementById("humidityIcon");
+  renderImage(humidityContainer, humidityIcon);
+
+  const precipitationContainer = document.getElementById("precipitationIcon");
+  renderImage(precipitationContainer, precipitationIcon);
+
+  const windSpeedContainer = document.getElementById("windSpeedIcon");
+  renderImage(windSpeedContainer, windSpeedIcon);
+}
+renderIcons();
+getWeatherData("new york city");
