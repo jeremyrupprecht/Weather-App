b91d617208678e9512c6407e0599651f571bba57
Jeremy <jdavid.rupprecht@gmail.com>
2024-01-18 00:15:24 -0700

    Create buttons to view different hours for the hourly forecast

--- src/index.js
+++ src/index.js
@@ -7,61 +7,63 @@ import windSpeedIcon from "./images/windIcon.svg";
 // import backgroud from "./images/weatherBackground.jpg";
 // import testImage from "./images/testImage.png";
 // import getWeatherData from "./apiHandler";
+import leftArrowIcon from "./images/arrowLeft.svg";
+import rightArrowIcon from "./images/arrowRight.svg";
 
 // Weather Icons
 import clearSky from "./images/weatherCodeIcons/clearSky.svg";
-import drizzle from "./images/weatherCodeIcons/drizzle.svg";
-import foggy from "./images/weatherCodeIcons/foggy.svg";
-import freezingDrizzle from "./images/weatherCodeIcons/freezingDrizzle.svg";
-import freezingRain from "./images/weatherCodeIcons/freezingRain.svg";
 import partlyCloudy from "./images/weatherCodeIcons/partlyCloudy.svg";
+import foggy from "./images/weatherCodeIcons/foggy.svg";
+import drizzle from "./images/weatherCodeIcons/drizzle.svg";
+import freezingDrizzle from "./images/weatherCodeIcons/freezingDrizzle.svg";
 import rain from "./images/weatherCodeIcons/rain.svg";
-import rainShowers from "./images/weatherCodeIcons/rainShowers.svg";
+import freezingRain from "./images/weatherCodeIcons/freezingRain.svg";
 import snowfall from "./images/weatherCodeIcons/snowfall.svg";
 import snowGrains from "./images/weatherCodeIcons/snowGrains.svg";
+import rainShowers from "./images/weatherCodeIcons/rainShowers.svg";
 import snowShowers from "./images/weatherCodeIcons/snowShowers.svg";
 import thunderStormBoth from "./images/weatherCodeIcons/thunderStormBoth.svg";
 
 // Cropped Weather Icons
 import clearSkyC from "./images/weatherCodeIconsCropped/clearSkyCropped.svg";
-import drizzleC from "./images/weatherCodeIconsCropped/drizzleCropped.svg";
-import foggyC from "./images/weatherCodeIconsCropped/foggyCropped.svg";
-import freezingDrizzleC from "./images/weatherCodeIconsCropped/freezingDrizzleCropped.svg";
-import freezingRainC from "./images/weatherCodeIconsCropped/freezingRainCropped.svg";
 import partlyCloudyC from "./images/weatherCodeIconsCropped/partlyCloudyCropped.svg";
+import foggyC from "./images/weatherCodeIconsCropped/foggyCropped.svg";
+import drizzleC from "./images/weatherCodeIconsCropped/drizzleCropped.svg";
+import freezingDrizzleC from "./images/weatherCodeIconsCropped/freezingDrizzleCropped.svg";
 import rainC from "./images/weatherCodeIconsCropped/rainCropped.svg";
-import rainShowersC from "./images/weatherCodeIconsCropped/rainShowersCropped.svg";
+import freezingRainC from "./images/weatherCodeIconsCropped/freezingRainCropped.svg";
 import snowfallC from "./images/weatherCodeIconsCropped/snowfallCropped.svg";
 import snowGrainsC from "./images/weatherCodeIconsCropped/snowGrainsCropped.svg";
+import rainShowersC from "./images/weatherCodeIconsCropped/rainShowersCropped.svg";
 import snowShowersC from "./images/weatherCodeIconsCropped/snowShowersCropped.svg";
 import thunderStormBothC from "./images/weatherCodeIconsCropped/thunderStormBothCropped.svg";
 
 const weatherIcons = [
   clearSky,
-  drizzle,
-  foggy,
-  freezingDrizzle,
-  freezingRain,
   partlyCloudy,
+  foggy,
+  drizzle,
+  freezingDrizzle,
   rain,
-  rainShowers,
+  freezingRain,
   snowfall,
   snowGrains,
+  rainShowers,
   snowShowers,
   thunderStormBoth,
 ];
 
 const weatherIconsCropped = [
   clearSkyC,
-  drizzleC,
-  foggyC,
-  freezingDrizzleC,
-  freezingRainC,
   partlyCloudyC,
+  foggyC,
+  drizzleC,
+  freezingDrizzleC,
   rainC,
-  rainShowersC,
+  freezingRainC,
   snowfallC,
   snowGrainsC,
+  rainShowersC,
   snowShowersC,
   thunderStormBothC,
 ];
@@ -113,6 +115,13 @@ function renderIcons() {
 
   const windSpeedContainer = document.getElementById("windSpeedIcon");
   renderImage(windSpeedContainer, windSpeedIcon);
+
+  // Hourly forecast arrows
+  const leftIconContainer = document.getElementById("leftArrow");
+  renderImage(leftIconContainer, leftArrowIcon);
+
+  const rightIconContainer = document.getElementById("rightArrow");
+  renderImage(rightIconContainer, rightArrowIcon);
 }
 
 function interpretWeatherCode(code, cropped) {
@@ -167,11 +176,15 @@ function interpretWeatherCode(code, cropped) {
   }
 }
 
-function renderForecastIcons(iconCodes) {
-  const dayCardsCollection = document.getElementsByClassName("dayCard");
-  for (let i = 0; i < dayCardsCollection.length; i += 1) {
+function renderForecastIcons(iconCodes, hourly) {
+  let cardsCollection = document.getElementsByClassName("dayCard");
+  if (hourly) {
+    cardsCollection = document.getElementsByClassName("hourCard");
+  }
+
+  for (let i = 0; i < cardsCollection.length; i += 1) {
     const dayIconContainer =
-      dayCardsCollection[i].children[dayCardsCollection[i].children.length - 1];
+      cardsCollection[i].children[cardsCollection[i].children.length - 1];
     renderImage(dayIconContainer, interpretWeatherCode(iconCodes[i], false));
   }
 }
@@ -181,7 +194,50 @@ function renderCurrentIcon(iconCode) {
   renderImage(mainIconContainer, interpretWeatherCode(iconCode, true));
 }
 
+function toggleForecastCards(toggleHourlyCards) {
+  const dailyForecastCardsGrid = document.querySelector(".dailyForecastGrid");
+  const hourlyForecastCardsGrid = document.querySelector(".hourlyForecastGrid");
+  const dailyButton = document.getElementById("dailyButton");
+  const hourlyButton = document.getElementById("hourlyButton");
+  const hoursSelectionButtons = document.getElementById(
+    "hoursSelectionContainer",
+  );
+
+  if (toggleHourlyCards) {
+    dailyForecastCardsGrid.classList.remove("show");
+    hourlyForecastCardsGrid.classList.add("show");
+    dailyButton.classList.remove("border");
+    hourlyButton.classList.add("border");
+    hoursSelectionButtons.classList.add("show");
+  } else {
+    dailyForecastCardsGrid.classList.add("show");
+    hourlyForecastCardsGrid.classList.remove("show");
+    dailyButton.classList.add("border");
+    hourlyButton.classList.remove("border");
+    hoursSelectionButtons.classList.remove("show");
+  }
+}
+
+function setupListeners() {
+  const dailyForecastButton = document.getElementById("dailyButton");
+  dailyForecastButton.addEventListener("click", () => {
+    toggleForecastCards(false);
+  });
+
+  const hourlyForecastButton = document.getElementById("hourlyButton");
+  hourlyForecastButton.addEventListener("click", () => {
+    toggleForecastCards(true);
+  });
+}
+
+// function reRenderInCelciusOrFahrenheit() {
+
+// }
+
 renderIcons();
-renderCurrentIcon(51);
-renderForecastIcons([71, 77, 80, 85, 95, 61, 66]);
+renderCurrentIcon(56);
+renderForecastIcons([71, 77, 80, 85, 95, 61, 66], false);
+renderForecastIcons([71, 77, 80, 85, 95, 61, 66, 0], true);
+
+setupListeners();
 // getWeatherData("new york city");
